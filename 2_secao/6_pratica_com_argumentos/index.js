b5bd00fb537b521de6ac4390c096100cf2f68e47
vitor1hugo <88462529+vitor1hugo@users.noreply.github.com>
2023-09-05 18:20:40 -0300

    Update index.js

--- 2_secao/6_pratica_com_argumentos/index.js
+++ 2_secao/6_pratica_com_argumentos/index.js
@@ -1,9 +1,12 @@
-const minimist = require('minimist')
-
 //externo
-const args = minimist(process.argv.slice(2))
+const minimist = require('minimist')
 
 //interno
 const soma = require('./soma').soma
 
-soma(2,2)
+const args = minimist(process.argv.slice(2))
+
+const a = parseInt(args['a'])
+const b = parseInt(args['b'])
+
+soma(a, b)
